bcb95efb0f9623964c8854fd245d20e913a2d1f0
Ngakk <ngakwastaken@gmail.com>
2018-05-20 13:18:40 -0500

    Post Processing Stack
    Cambio de luz y colores en escena

---
+++ Assets/_GameFiles/Scenes/OutOfBounds.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour {
+
+	void OnTriggerExit(Collider _col)
+	{
+		Destroy(_col.gameObject);
+	}
+}
